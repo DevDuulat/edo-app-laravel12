76f2c83d461b735147f05c3c612398ee4d6ddf17
DevDuulat <duulat.bekm@mail.ru>
2025-10-09 17:55:07 +0600

    feat(core): Add document management feature

---
+++ app/Http/Controllers/DocumentController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\DocumentType;
+use App\Enums\WorkflowStatus;
+use App\Http\Requests\StoreDocumentRequest;
+use App\Models\Document;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+
+class DocumentController extends Controller
+{
+    public function index()
+    {
+        $documents = Document::query()->paginate();
+        return view('admin.documents.index', compact('documents'));
+    }
+
+    public function create()
+    {
+        $users = User::query()->paginate(10);
+
+        return view('admin.documents.create', compact('users'));
+    }
+
+
+    public function store(StoreDocumentRequest $request)
+    {
+        $data = $request->validated();
+
+        // конвертируем дату из d/m/Y в Y-m-d
+        $data['due_date'] = Carbon::createFromFormat('d/m/Y', $data['due_date'])->format('Y-m-d');
+
+        // добавляем автора и статусы документа
+        $data['user_id'] = auth()->id();
+        $data['document_type'] = DocumentType::internal->value;
+        $data['workflow_status'] = WorkflowStatus::draft->value;
+        $data['slug'] = Str::slug($data['title']);
+
+        // создаём документ
+        Document::create($data);
+
+        return redirect()
+            ->route('admin.documents.index')
+            ->with('success', 'Документ успешно создан.');
+    }
+
+
+
+    public function show(Document $document)
+    {
+        return view('admin.documents.show', compact('document'));
+    }
+
+    public function edit(Document $document)
+    {
+        return view('admin.documents.edit', compact('document'));
+    }
+    public function update(Request $request, Document $document)
+    {
+
+    }
+
+    public function destroy(Document $document)
+    {
+
+    }
+}
