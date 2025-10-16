0dd936ea628a7d29db5432e4302cb4a8868a5c54
DevDuulat <duulat.bekm@mail.ru>
2025-10-16 14:40:06 +0600

    feat(core): Enable PhotoSwipe for dynamic image galleries

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -166,26 +166,9 @@
 <link rel="stylesheet" href="https://unpkg.com/photoswipe@5.4.4/dist/photoswipe.css" />
 <link rel="stylesheet" href="https://unpkg.com/photoswipe-dynamic-caption-plugin/photoswipe-dynamic-caption-plugin.css" />
 
-<script type="module">
-    import PhotoSwipeLightbox from 'https://unpkg.com/photoswipe@5.4.4/dist/photoswipe-lightbox.esm.js';
-    import PhotoSwipeDynamicCaption from 'https://unpkg.com/photoswipe-dynamic-caption-plugin/photoswipe-dynamic-caption-plugin.esm.js';
-
-    // Поддержка всех галерей на странице
-    const galleries = document.querySelectorAll('.pswp-gallery');
-
-    if (galleries.length > 0) {
-        galleries.forEach(gallery => {
-            const lightbox = new PhotoSwipeLightbox({
-                gallery: gallery,
-                children: 'a[data-pswp-width]',
-                pswpModule: () => import('https://unpkg.com/photoswipe@5.4.4/dist/photoswipe.esm.js'),
-            });
-
-            new PhotoSwipeDynamicCaption(lightbox, { type: 'auto' });
-            lightbox.init();
-        });
-    }
-</script>
+@if (Route::is('admin.employees.show') || Route::is('admin.employees.edit'))
+    @vite('resources/js/photoswipe-init.js')
+@endif
 
 </body>
 </html>
