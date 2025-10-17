3416f75fbdeb53a78460239a8c954721216aad30
DevDuulat <duulat.bekm@mail.ru>
2025-10-17 09:38:06 +0600

    Refactor(departments): Reorder elements for better UI
    Co-authored-by: Duulat <dulatik.zippo@gmail.com>

--- resources/views/admin/departments/edit.blade.php
+++ resources/views/admin/departments/edit.blade.php
@@ -7,6 +7,9 @@
                 <flux:breadcrumbs.item>Редактирование Департамента</flux:breadcrumbs.item>
             </flux:breadcrumbs>
         </div>
+        <h3 class="mb-2 text-2xl leading-none tracking-tight text-center text-gray-900 md:text-2xl dark:text-white">
+            Редактирование департамента
+        </h3>
 
         <div class="px-5 py-5 rounded-xl border border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
         <form action="{{ route('admin.departments.update', $department->id) }}" method="POST" class="grid gap-5 max-w-lg">
