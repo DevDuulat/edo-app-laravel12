13d05c9e735581dca7945c09c4e09d59ef976744
DevDuulat <duulat.bekm@mail.ru>
2025-10-17 09:42:01 +0600

    Refactor(employees): Update employee form titles
    The titles for employee creation, editing, and view pages were changed.
    
    Co-authored-by: Duulat <dulatik.zippo@gmail.com>

--- resources/views/admin/employees/edit.blade.php
+++ resources/views/admin/employees/edit.blade.php
@@ -9,6 +9,9 @@
                 <flux:breadcrumbs.item>Редактирование Сотрудника</flux:breadcrumbs.item>
             </flux:breadcrumbs>
         </div>
+        <h3 class="mb-2 text-2xl leading-none tracking-tight text-center text-gray-900 md:text-2xl dark:text-white">
+            Редактирование сотрудника
+        </h3>
 
         <div class="px-5 py-5 rounded-xl border border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <form action="{{ route('admin.employees.update', $employee) }}" method="POST" enctype="multipart/form-data" class="grid gap-5 max-w-5xl md:grid-cols-2" x-data="employeeForm()">
--- resources/views/admin/employees/index.blade.php
+++ resources/views/admin/employees/index.blade.php
@@ -17,6 +17,9 @@
                Добавить сотрудников
             </a>
         </div>
+        <h3 class="mb-2 text-2xl leading-none tracking-tight text-center text-gray-900 md:text-2xl dark:text-white">
+            Сотрудники
+        </h3>
 
         <div class="relative overflow-x-auto rounded-xl border border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
 
