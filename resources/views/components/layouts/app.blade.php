a596619b0e8ef0d703ce1597553c55e58da816d7
DevDuulat <duulat.bekm@mail.ru>
2025-10-17 17:41:56 +0600

    Refactor(core): Update app layout to use header component
    Co-authored-by: Duulat <dulatik.zippo@gmail.com>

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <x-layouts.app.sidebar :title="$title ?? null">
-    <flux:main>
-        {{ $slot }}
+    <x-layouts.app.header/>
 
+    <flux:main class="!p-0">
+        {{ $slot }}
     </flux:main>
 </x-layouts.app.sidebar>
