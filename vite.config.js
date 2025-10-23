cddb1b744c3d4afd4e3e5b391d0bcf5bab911919
DevDuulat <duulat.bekm@mail.ru>
2025-10-23 09:41:31 +0600

    feat(core): Update Vite config to include dropdown CSS

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,12 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js',     'resources/js/photoswipe-init.js', ],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/css/dropdown.css',
+                'resources/js/photoswipe-init.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
