c7172be67ac0b360a493c00017108b75b65277da
DevDuulat <duulat.bekm@mail.ru>
2025-10-23 09:43:55 +0600

    feat(actions): Add folder context menu and icon
    Adds folder-context-menu.blade.php for folder actions and folder-icon.blade.php.

---
+++ resources/views/components/actions/folder-context-menu.blade.php
@@ -0,0 +1,15 @@
+<div id="dropdown" data-folder-id="">
+    <button id="createDocBtn" class="dropdown-btn">
+        <flux:icon.plus-circle class="w-5 h-5 me-2" />
+        Создать документ
+    </button>
+    <button id="renameBtn" class="dropdown-btn">
+        <flux:icon.pencil-square class="w-5 h-5 me-2" />
+        Переименовать
+    </button>
+    <div class="divider"></div>
+    <button id="deleteBtn" class="dropdown-btn">
+        <flux:icon.trash class="w-5 h-5 me-2" />
+        Удалить
+    </button>
+</div>
