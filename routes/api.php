8b9cc2c34b592431514a9e62ae28557604c5295e
DevDuulat <duulat.bekm@mail.ru>
2025-10-24 13:24:23 +0600

    feat(routes): Add user routes with authentication and token verification middleware

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,16 @@
 <?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SsoController;
+use App\Http\Controllers\UserSyncController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::get('/sso/verify', [SsoController::class, 'verify']);
+Route::post('/sync-roles-permissions', [UserSyncController::class, 'syncRolesPermissions'])
+    ->middleware('verify.sync.token');
+Route::post('/sync-email', [UserSyncController::class, 'syncEmail'])
+    ->middleware('verify.sync.token');
