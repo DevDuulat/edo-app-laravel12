b1d4572361b944f5b267c56c46a266568ed7c958
DevDuulat <duulat.bekm@mail.ru>
2025-10-24 13:21:55 +0600

    feat(middleware): Add VerifySyncToken middleware for token validation

---
+++ app/Http/Middleware/VerifySyncToken.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class VerifySyncToken
+{
+   
+    public function handle(Request $request, Closure $next): Response
+    {
+        $token = $request->bearerToken();
+
+        if ($token !== env('SECRET_TOKEN')) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $next($request);
+    }
+}
