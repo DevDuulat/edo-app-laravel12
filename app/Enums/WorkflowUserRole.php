98893a77ed6c2fd9565f768161a9c79a242ff6a0
DevDuulat <duulat.bekm@mail.ru>
2025-10-27 22:05:14 +0600

    feat(core): Add WorkflowUserRole enum

---
+++ app/Enums/WorkflowUserRole.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Enums;
+
+enum WorkflowUserRole: int
+{
+    case Initiator = 0;
+    case Approver = 1;
+    case Executor = 2;
+    case Observer = 3;
+    case Participant = 4;
+
+    public function label(): string
+    {
+        return match($this) {
+            self::Initiator => 'Инициатор',
+            self::Approver => 'Согласующий',
+            self::Executor => 'Исполнитель',
+            self::Observer => 'Наблюдатель',
+            self::Participant => 'Участник',
+        };
+    }
+
+    public function slug(): string
+    {
+        return match($this) {
+            self::Initiator => 'initiator',
+            self::Approver => 'approver',
+            self::Executor => 'executor',
+            self::Observer => 'observer',
+            self::Participant => 'participant',
+        };
+    }
+}
