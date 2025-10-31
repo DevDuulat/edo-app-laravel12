4422ca0fa46f14ee0a8671557a65df423d92644c
DevDuulat <duulat.bekm@mail.ru>
2025-10-31 09:57:16 +0600

    fix(folder-context-menu): Fix folder open button
    Rename createDocBtn to openDocBtn and update icon.

--- resources/views/components/actions/folder-context-menu.blade.php
+++ resources/views/components/actions/folder-context-menu.blade.php
@@ -1,4 +1,9 @@
 <div id="dropdown" data-folder-id="">
+    <button id="openDocBtn" class="dropdown-btn">
+        <flux:icon.folder-open class="w-5 h-5 me-2" />
+        Открыть папку
+    </button>
+{{--TODO Надо сделать возможность октрыть папку  --}}
     <button id="createDocBtn" class="dropdown-btn">
         <flux:icon.plus-circle class="w-5 h-5 me-2" />
         Создать документ
