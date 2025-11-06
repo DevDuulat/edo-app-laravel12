140fb2d6b622abecaab6d1b1ae471d3ef6240f53
DevDuulat <duulat.bekm@mail.ru>
2025-11-06 11:45:27 +0600

    fix(db): Update document_templates table schema

--- database/migrations/2025_10_30_114637_create_document_templates_table.php
+++ database/migrations/2025_10_30_114637_create_document_templates_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('slug')->unique();
             $table->string('description')->nullable();
+            $table->longText('content')->nullable();
             $table->boolean('active')->default(true);
             $table->timestamps();
         });
