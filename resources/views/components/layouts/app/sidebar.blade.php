ca71b0fca6de174b49dfee1874c1e3ace39cdeeb
DevDuulat <duulat.bekm@mail.ru>
2025-11-07 10:13:38 +0600

    refactor(sidebar): Simplify sidebar component

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -65,59 +65,18 @@
         <flux:sidebar.item icon="archive-box" wire:navigate class="py-1">
             Архив
         </flux:sidebar.item>
+
+        {{--Завершенные рабочие процессы--}}
+        <flux:sidebar.item icon="trash" wire:navigate class="py-1">
+            Корзина
+        </flux:sidebar.item>
     </flux:sidebar.group>
     </flux:sidebar.nav>
     <flux:spacer />
 
-    <!-- Desktop User Menu -->
-    <flux:dropdown class="hidden lg:block" position="bottom" align="start">
-        <flux:profile
-                :name="auth()->user()->name"
-                :initials="auth()->user()->initials()"
-                icon:trailing="chevrons-up-down"
-        />
 
-        <flux:menu class="w-[220px]">
-            <flux:menu.radio.group>
-                <div class="p-0 text-sm font-normal">
-                    <div class="flex items-center gap-1 px-1 py-1 text-start text-sm">
-                                <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
-                                    <span
-                                            class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white"
-                                    >
-                                        {{ auth()->user()->initials() }}
-                                    </span>
-                                </span>
-
-                        <div class="grid flex-1 text-start text-sm leading-tight">
-                            <span class="truncate font-semibold">{{ auth()->user()->name }}</span>
-                            <span class="truncate text-xs">{{ auth()->user()->email }}</span>
-                        </div>
-                    </div>
-                </div>
-            </flux:menu.radio.group>
-
-            <flux:menu.separator />
-
-            <flux:menu.radio.group>
-                <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate class="py-1">
-                    {{ __('Настройки') }}
-                </flux:menu.item>
-            </flux:menu.radio.group>
-
-            <flux:menu.separator />
-
-            <form method="POST" action="{{ route('logout') }}" class="w-full">
-                @csrf
-                <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full py-1">
-                    {{ __('Выйти') }}
-                </flux:menu.item>
-            </form>
-        </flux:menu>
-    </flux:dropdown>
 </flux:sidebar>
 
-<!-- Mobile User Menu -->
 <flux:header class="lg:hidden">
     <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
     <flux:spacer />
