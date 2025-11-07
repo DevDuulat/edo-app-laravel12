0d995314cca933052ee6b3e99cbbcb4570f03c20
DevDuulat <duulat.bekm@mail.ru>
2025-11-07 10:12:21 +0600

    refactor(request): Update StoreDocumentRequest rules
    Updates document validation rules, slug requirement.

--- app/Http/Requests/StoreDocumentRequest.php
+++ app/Http/Requests/StoreDocumentRequest.php
@@ -6,9 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreDocumentRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
@@ -20,8 +17,10 @@ class StoreDocumentRequest extends FormRequest
         return [
             'folder_id' => ['nullable', 'integer', 'exists:folders,id'],
             'title' => 'required|string|max:255|unique:documents,title,' . ($this->document?->id ?? ''),
+            'slug' => 'required|string|max:255|unique:documents,title,' . ($this->document?->id ?? ''),
             'due_date' => 'required|date|after_or_equal:today',
             'comment' => 'nullable|string',
+            'content' => ['nullable', 'string'],
         ];
     }
 
