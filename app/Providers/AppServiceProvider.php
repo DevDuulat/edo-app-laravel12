fed5cddd67910aeaaf4efe308d4419398994ce63
DevDuulat <duulat.bekm@mail.ru>
2025-11-10 17:42:20 +0600

    refactor(core): Update AppServiceProvider
    Updates service provider and add view composer for sidebar.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,25 +2,27 @@
 
 namespace App\Providers;
 
+use App\Services\IncomingDocumentService;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\View;
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         $loader = \Illuminate\Foundation\AliasLoader::getInstance();
         $loader->alias('Debugbar', \Barryvdh\Debugbar\Facades\Debugbar::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        URL::forceScheme('https');
+        View::composer('components.layouts.app.sidebar', function ($view) {
+            $incomingCount = app(IncomingDocumentService::class)->countIncomingDocuments();
+            $view->with('incomingCount', $incomingCount);
+        });
+
+//        Выключить в проде
+//        Включить локально
+//        URL::forceScheme('https');
     }
 }
