56b2d4006a38677a3748c0ebd8a16b6aeb782496
DevDuulat <duulat.bekm@mail.ru>
2025-12-03 17:50:04 +0600

    refactor(routes): Define resource routes and workflow actions

--- routes/web.php
+++ routes/web.php
@@ -56,24 +56,10 @@ Route::middleware(['auth'])->group(function () {
 
 
    Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
-//       Route::resource('departments', DepartmentController::class)
-//        ->middleware('permission:edo-hr-department');
        Route::resource('categories', CategoryController::class);
        Route::resource('departments', DepartmentController::class);
        Route::resource('employees', EmployeeController::class);
        Route::resource('folders', FolderController::class);
-       Route::prefix('folders')->name('folders.')->group(function () {
-           Route::patch('{id}/archive', [FolderController::class, 'archive'])->name('archive');
-           Route::patch('{id}/unarchive', [FolderController::class, 'unarchive'])->name('unarchive');
-           Route::patch('{id}/trash', [FolderController::class, 'trash'])->name('trash');
-           Route::patch('{id}/restore', [FolderController::class, 'restore'])->name('restore');
-           Route::delete('{id}/delete', [FolderController::class, 'delete'])->name('forceDelete');
-           Route::patch('{id}/rename', [FolderController::class, 'rename'])->name('rename');
-           Route::post('{id}/copy', [FolderController::class, 'copy'])->name('copy');
-           Route::patch('move/{id}', [FolderController::class, 'move'])->name('move');
-       });
-       Route::get('archive', [ArchiveController::class, 'index'])->name('archive.index');
-       Route::get('trash', [TrashController::class, 'index'])->name('trash.index');
        Route::resource('documents', DocumentController::class);
        Route::resource('workflows', WorkflowController::class);
        Route::get('/outgoing/workflows', [WorkflowController::class, 'outgoing'])->name('outgoing.workflows');
@@ -86,6 +72,28 @@ Route::middleware(['auth'])->group(function () {
        Route::resource('document-templates', DocumentTemplateController::class);
        Route::get('departments/{department}/employees', [EmployeeController::class, 'byDepartment'])
         ->name('employees.byDepartment');
+       Route::get('archive', [ArchiveController::class, 'index'])->name('archive.index');
+       Route::get('trash', [TrashController::class, 'index'])->name('trash.index');
+       Route::prefix('folders')->name('folders.')->group(function () {
+           Route::patch('{id}/archive', [FolderController::class, 'archive'])->name('archive');
+           Route::patch('{id}/unarchive', [FolderController::class, 'unarchive'])->name('unarchive');
+           Route::patch('{id}/trash', [FolderController::class, 'trash'])->name('trash');
+           Route::patch('{id}/restore', [FolderController::class, 'restore'])->name('restore');
+           Route::delete('{id}/delete', [FolderController::class, 'delete'])->name('forceDelete');
+           Route::patch('{id}/rename', [FolderController::class, 'rename'])->name('rename');
+           Route::post('{id}/copy', [FolderController::class, 'copy'])->name('copy');
+           Route::patch('move/{id}', [FolderController::class, 'move'])->name('move');
+       });
+       Route::prefix('documents')->name('documents.')->group(function () {
+           Route::patch('{id}/archive', [DocumentController::class, 'archive'])->name('archive');
+           Route::patch('{id}/unarchive', [DocumentController::class, 'unarchive'])->name('unarchive');
+           Route::patch('{id}/trash', [DocumentController::class, 'trash'])->name('trash');
+           Route::patch('{id}/restore', [DocumentController::class, 'restore'])->name('restore');
+           Route::delete('{id}/delete', [DocumentController::class, 'delete'])->name('forceDelete');
+           Route::patch('{id}/rename', [DocumentController::class, 'rename'])->name('rename');
+           Route::post('{id}/copy', [DocumentController::class, 'copy'])->name('copy');
+           Route::patch('move/{id}', [DocumentController::class, 'move'])->name('move');
+       });
 
    });
 
