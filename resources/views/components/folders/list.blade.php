3b330086fb6de669235c133f7ae184d1e1f6baa2
DevDuulat <duulat.bekm@mail.ru>
2025-12-04 11:58:22 +0600

    Refactor(core): Refactor folder and document actions
    Refactors folder and document action menus using components for better code organization and maintainability. The folder-dropdown.blade.php component was updated, and the folder actions dropdown logic was moved into reusable x-actions components. Old code in list.blade.php component was removed.

--- resources/views/components/folders/list.blade.php
+++ resources/views/components/folders/list.blade.php
@@ -61,76 +61,7 @@
                 </td>
 
                 <td class="px-6 py-4 text-left">
-                    <flux:dropdown align="end">
-                        <flux:button
-                                icon="ellipsis-vertical"
-                                class="w-9 h-9 flex items-center justify-center rounded-xl border border-gray-200 bg-white hover:bg-gray-50 transition shadow-sm"
-                        />
-                        <flux:menu>
-                            <flux:menu.item
-                                    icon="eye"
-                                    href="{{ route('admin.documents.index', ['parent_id' => $folder->id]) }}"
-                            >
-                                Открыть
-                            </flux:menu.item>
-                            <flux:menu.item
-                                    icon="share"
-                                    href="#"
-                                    onclick="openShareModal('{{ $folder->slug }}')"
-                            >
-                                Поделиться
-                            </flux:menu.item>
-                            <flux:menu.item
-                                    icon="lock-closed"
-                                    href="#"
-                                    onclick="copyFolder({{ $folder->id }})"
-                            >
-                                Настройка прав
-                            </flux:menu.item>
-                            <flux:menu.item
-                                    icon="clipboard-document"
-                                    href="#"
-                                    onclick="copyFolder({{ $folder->id }})"
-                            >
-                                Копировать
-                            </flux:menu.item>
-                            <flux:menu.item
-                                    icon="folder-arrow-down"
-                                    href="#"
-                                    onclick="moveFolder({{ $folder->id }})"
-                            >
-                                Переместить
-                            </flux:menu.item>
-                            <flux:menu.item
-                                    icon="pencil-square"
-                                    href="#"
-                                    onclick="renameFolder({{ $folder->id }})"
-                            >
-                                Переименовать
-                            </flux:menu.item>
-                            <flux:menu.item
-                                    icon="archive-box"
-                                    onclick="archiveFolder({{ $folder->id }})"
-                            >
-                                В архив
-                            </flux:menu.item>
-                            <flux:menu.item
-                                    icon="arrow-path"
-                                    href="#"
-                                    onclick="restoreFolder({{ $folder->id }})"
-                            >
-                                Восстановить
-                            </flux:menu.item>
-                            <flux:menu.item
-                                    icon="trash"
-                                    href="#"
-                                    variant="danger"
-                                    onclick="trashFolder({{ $folder->id }})"
-                            >
-                                Удалить
-                            </flux:menu.item>
-                        </flux:menu>
-                    </flux:dropdown>
+                    <x-actions.folder-dropdown :folder="$folder" />
                 </td>
             </tr>
 
