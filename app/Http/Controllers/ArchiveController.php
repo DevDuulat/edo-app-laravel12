12a7c270a0742c963794892958ab8b02024a6f0a
DevDuulat <duulat.bekm@mail.ru>
2025-12-04 17:34:56 +0600

    Refactor(controllers): Add category filter to archive/trash

--- app/Http/Controllers/ArchiveController.php
+++ app/Http/Controllers/ArchiveController.php
@@ -20,12 +20,19 @@ class ArchiveController extends Controller
     public function index(Request $request)
     {
         $parentId = $request->query('parent_id');
+        $categoryId = $request->query('category_id');
+        $date = $request->query('date');
+
         $data = $this->folderDocumentService->getFolderData(
             parentId: $parentId,
-            status: Status::archived
+            status: Status::archived,
+            categoryId: $categoryId,
+            date: $date
         );
 
-        return view('admin.folders.index', $data);
+        $data['categories'] = Category::orderBy('name')->get();
+
+        return view('admin.documents.index', $data);
     }
 
 }
