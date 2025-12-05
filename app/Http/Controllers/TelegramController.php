5cb9f3ee494b9d32d53c671b2a24e58ff25c4408
DevDuulat <duulat.bekm@mail.ru>
2025-12-05 17:16:05 +0600

    fix(telegram): Log webhook updates and chat messages
    Log Telegram webhook updates for debugging and log chat ID and text for message processing.

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DefStudio\Telegraph\Telegraph;
+use Illuminate\Support\Facades\Log;
 
 class TelegramController extends Controller
 {
     public function webhook(Request $request)
     {
         $update = $request->all();
+        Log::info('Webhook received', $update);
 
         if(isset($update['message'])) {
             $chat_id = $update['message']['chat']['id'];
             $text = $update['message']['text'] ?? '';
+
+            Log::info("Chat: $chat_id, Text: $text");
 
             if($text === '/start') {
                 Telegraph::chat($chat_id)->message('Привет! Спасибо за запуск бота')->send();
