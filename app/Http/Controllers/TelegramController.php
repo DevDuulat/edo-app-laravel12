4578e3fe2779d9fbb4fb9e3622738091226f7a8d
DevDuulat <duulat.bekm@mail.ru>
2025-12-05 17:33:26 +0600

    fix(telegram): Handle /start command and chat creation
    Update TelegramController to properly handle the /start
    command and create TelegraphChat records. Uses firstOrCreate
    to ensure chats are not duplicated.

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use DefStudio\Telegraph\Telegraph;
+use DefStudio\Telegraph\Models\TelegraphChat;
 use Illuminate\Support\Facades\Log;
 
 class TelegramController extends Controller
@@ -13,18 +13,23 @@ class TelegramController extends Controller
         $update = $request->all();
         Log::info('Webhook received', $update);
 
-        if(isset($update['message'])) {
+        if (isset($update['message'])) {
             $chat_id = $update['message']['chat']['id'];
             $text = $update['message']['text'] ?? '';
+            $first_name = $update['message']['chat']['first_name'] ?? 'User';
 
             Log::info("Chat: $chat_id, Text: $text");
 
-            if($text === '/start') {
-                Telegraph::chat($chat_id)->message('Привет! Спасибо за запуск бота')->send();
+            $chat = TelegraphChat::firstOrCreate(
+                ['chat_id' => $chat_id],
+                ['title' => $first_name]
+            );
+
+            if ($text === '/start') {
+                $chat->message('Привет! Спасибо за запуск бота')->send();
             }
         }
 
         return response()->json(['ok' => true]);
     }
-
 }
