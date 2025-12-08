d96e4c6bd0b16f2972360829d85fbc00fb9b08a9
DevDuulat <duulat.bekm@mail.ru>
2025-12-08 17:37:42 +0600

    refactor(telegram): Migrate TelegramController to WebhookHandler
    Refactor TelegramController to extend WebhookHandler and implement `start()` method for initial welcome message. This removes manual webhook processing logic.

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -2,34 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use DefStudio\Telegraph\Models\TelegraphChat;
-use Illuminate\Support\Facades\Log;
+use DefStudio\Telegraph\Handlers\WebhookHandler;
 
-class TelegramController extends Controller
+class TelegramController extends WebhookHandler
 {
-    public function webhook(Request $request)
-    {
-        $update = $request->all();
-        Log::info('Webhook received', $update);
-
-        if (isset($update['message'])) {
-            $chat_id = $update['message']['chat']['id'];
-            $text = $update['message']['text'] ?? '';
-            $first_name = $update['message']['chat']['first_name'] ?? 'User';
-
-            Log::info("Chat: $chat_id, Text: $text");
-
-            $chat = TelegraphChat::firstOrCreate(
-                ['chat_id' => $chat_id],
-                ['title' => $first_name]
-            );
-
-            if ($text === '/start') {
-                $chat->message('Привет! Спасибо за запуск бота')->send();
-            }
-        }
-
-        return response()->json(['ok' => true]);
-    }
+   public function start():void
+   {
+       $this->reply("Добро пожаловать!");
+   }
 }
