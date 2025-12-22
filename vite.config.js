b279a17183be325241b3a1c64e1f3b720fac6741
DevDuulat <duulat.bekm@mail.ru>
2025-12-22 13:23:10 +0600

    fix(vite): Update Vite config asset paths
    Adjust order of CSS and JS assets in vite.config.js for proper loading.

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,13 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/editor.css',
                 'resources/js/app.js',
                 'resources/css/dropdown.css',
                 'resources/js/photoswipe-init.js',
                 'resources/js/folder-interactions.js',
                 'resources/js/folders.js',
-                'resources/js/document-create-templates-editor.js',
-                'resources/js/document-edit-templates-editor.js',
+                'resources/js/editor.js',
                 'resources/js/documentForm.js',
                 'resources/js/wysiwyg.js',
                 'resources/js/workflow-tabs.js',
